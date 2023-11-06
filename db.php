342dfad114b3a93e31d8d8df19923f4472ed3b4b
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-06 15:08:42 +0100

    instanza di due oggetti movie

--- db.php
+++ db.php
@@ -1,1 +1,17 @@
-qui vanno i dati
+<?php
+
+// qui vanno i dati
+
+$movie1 = new Movie (
+    'immagine', 
+    'Biancaneve e i sette nani', 
+    'il primo lungometraggio animato della storia realizzato in animazione tradizionale, uscito nelle sale cinematografiche nel 1937. Quando nel 1934 Walt Disney rese pubblico al New York Times questo progetto, tutti lo definirono Disney’s Folly, ovvero “la follia di Disney”.'
+);
+
+$movie2 = new Movie (
+    'immagine',
+    'Pinocchio',
+    'uscito al cinema nel 1940 e penalizzato nella sua diffusione dallo scoppio della Seconda Guerra Mondiale. Come il precedente film, anche Pinocchio è basato su una fiaba: l’ispirazione è in questo caso il romanzo per ragazzi di fine 800 Le avventure di Pinocchio di Carlo Collodi.'
+);
+
+?>
