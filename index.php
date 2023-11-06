b15d7f446d6470588f90e45277951ae01612ffef
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-06 14:54:20 +0100

    inserimento costruttore

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
+
 class movie{
     public $image;
     public $title;
     public $description;
 }
+
+public function __construct($image, $title, $description){
+    $this->image = $image;
+    $this->title = $title;
+    $this->description = $description;
+
+}
 ?>
+
+
+
+
+
+
