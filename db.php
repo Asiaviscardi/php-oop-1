78dc647576ce6c51ddefdc431d1d36e5acd2284e
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-06 15:38:40 +0100

    prove stampa

--- db.php
+++ db.php
@@ -3,13 +3,13 @@
 // qui vanno i dati
 
 $movie1 = new Movie (
-    'immagine', 
+    'https://Biancaneve.jpg', 
     'Biancaneve e i sette nani', 
     'il primo lungometraggio animato della storia realizzato in animazione tradizionale, uscito nelle sale cinematografiche nel 1937. Quando nel 1934 Walt Disney rese pubblico al New York Times questo progetto, tutti lo definirono Disney’s Folly, ovvero “la follia di Disney”.'
 );
 
 $movie2 = new Movie (
-    'immagine',
+    'https://Pinocchio.jpg',
     'Pinocchio',
     'uscito al cinema nel 1940 e penalizzato nella sua diffusione dallo scoppio della Seconda Guerra Mondiale. Come il precedente film, anche Pinocchio è basato su una fiaba: l’ispirazione è in questo caso il romanzo per ragazzi di fine 800 Le avventure di Pinocchio di Carlo Collodi.'
 );
--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <?php
 
+require 'db.php';
+
 class Movie{
     public $image;
     public $title;
     public $description;
+    
+    public function __construct($image, $title, $description){
+        $this->image = $image;
+        $this->title = $title;
+        $this->description = $description;
+    }
+    
+    // Metodo per ottenere l'immagine del film:
+    
+    public function getImage(){
+        return $this->image;
+    }
+    
+    // Metodo per ottenere il titolo del film:
+    
+    public function getTitle(){
+        return $this->title;
+    }
+    
+    // Metodo per ottenere la descrizione del film:
+    
+    public function getDescription(){
+        return $this->description;
+    }
 }
 
-public function __construct($image, $title, $description){
-    $this->image = $image;
-    $this->title = $title;
-    $this->description = $description;
-}
 
-// Metodo per ottenere il titolo del film:
-
-public function getTitle(){
-    return $this->title;
-}
-
-// Metodo per ottenere la descrizione del film:
-
-public function getDescription(){
-    return $this->description;
-}
-
-?>    
+echo $movie1->getImage();
+echo 'titolo:'. $movie1->getTitle();
+echo 'descrizione:' . $movie1->getDescription();
+?>
