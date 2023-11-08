52bf6ef83d31cd66ce1a08d69b0aaa7b00efe154
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-08 16:34:01 +0100

    inserimento percorso corretto delle immagini

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ require_once __DIR__ . "/model/Movie.php";
 $movies = [
 
     new Movie (
-        'https://Biancaneve.jpg', 
+        'img/Biancaneve.jpg', 
         'Biancaneve e i sette nani', 
         'il primo lungometraggio animato della storia realizzato in animazione tradizionale, uscito nelle sale cinematografiche nel 1937. Quando nel 1934 Walt Disney rese pubblico al New York Times questo progetto, tutti lo definirono Disney’s Folly, ovvero “la follia di Disney”.'
     ),
 
     new Movie (
-        'https://Pinocchio.jpg',
+        'img/Pinocchio.jpg',
         'Pinocchio',
         'uscito al cinema nel 1940 e penalizzato nella sua diffusione dallo scoppio della Seconda Guerra Mondiale. Come il precedente film, anche Pinocchio è basato su una fiaba: l’ispirazione è in questo caso il romanzo per ragazzi di fine 800 Le avventure di Pinocchio di Carlo Collodi.'
     ),
@@ -42,7 +42,7 @@ $movies = [
     foreach($movies as $movie):
     ?>
     <div class="card m-4" style="width: 18rem;">
-    <img src="img/<?php echo $movie->image; ?>" class="card-img-top" alt="">
+    <img src="<?php echo $movie->image; ?>" class="card-img-top" alt="">
     </div>
     <div class="card-body">
         <h5 class="card-title"><?php  echo $movie->title; ?></h5>
