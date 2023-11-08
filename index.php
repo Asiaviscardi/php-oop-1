6d6bdbd83401890b57d245d9c003420c2859ce0c
Asiaviscardi <asia.viscardii@gmail.com>
2023-11-08 16:22:21 +0100

    modifiche

--- index.php
+++ index.php
@@ -1,41 +1,22 @@
 <?php
 
-require 'db.php';
+require_once __DIR__ . "/model/Movie.php";
 
-class Movie{
-    public $image;
-    public $title;
-    public $description;
-    
-    public function __construct($image, $title, $description){
-        $this->image = $image;
-        $this->title = $title;
-        $this->description = $description;
-    }
-    
-    // Metodo per ottenere l'immagine del film:
-    
-    public function getImage(){
-        return $this->image;
-    }
-    
-    // Metodo per ottenere il titolo del film:
-    
-    public function getTitle(){
-        return $this->title;
-    }
-    
-    // Metodo per ottenere la descrizione del film:
-    
-    public function getDescription(){
-        return $this->description;
-    }
-}
+$movies = [
 
+    new Movie (
+        'https://Biancaneve.jpg', 
+        'Biancaneve e i sette nani', 
+        'il primo lungometraggio animato della storia realizzato in animazione tradizionale, uscito nelle sale cinematografiche nel 1937. Quando nel 1934 Walt Disney rese pubblico al New York Times questo progetto, tutti lo definirono Disney’s Folly, ovvero “la follia di Disney”.'
+    ),
 
-echo $movie1->getImage();
-echo 'titolo:'. $movie1->getTitle();
-echo 'descrizione:' . $movie1->getDescription();
+    new Movie (
+        'https://Pinocchio.jpg',
+        'Pinocchio',
+        'uscito al cinema nel 1940 e penalizzato nella sua diffusione dallo scoppio della Seconda Guerra Mondiale. Come il precedente film, anche Pinocchio è basato su una fiaba: l’ispirazione è in questo caso il romanzo per ragazzi di fine 800 Le avventure di Pinocchio di Carlo Collodi.'
+    ),
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -46,35 +27,30 @@ echo 'descrizione:' . $movie1->getDescription();
 
     <meta charset="UTF-8">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
 
     <title>Film Disney</title>
 
-</head>
+  </head>
 
-<body>
-    <div class="container-fluid">
+  <body>
+    <div class="container">
     <?php
     foreach($movies as $movie):
     ?>
     <div class="card m-4" style="width: 18rem;">
-    <img src="img/<?php echo $movie->getImage?>" class="card-img-top" alt="<?php echo $movie->image?>">
+    <img src="img/<?php echo $movie->image; ?>" class="card-img-top" alt="">
     </div>
     <div class="card-body">
-        <h5 class="card-title"><?php  echo $movie->getTitle ?></h5>
-        <p class="card-text"><?php  echo implode(", " , $movie->getDescription) ?></p>
+        <h5 class="card-title"><?php  echo $movie->title; ?></h5>
+        <p class="card-text"><?php  echo $movie->description; ?></p>
 
     </div>
     <?php
     endforeach;
     ?>
-
-
-    </div>
-    
-</body>
-
+  </body>
 </html>
---
+++ model/Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+class Movie{
+  public $image;
+  public $title;
+  public $description;
+
+  public function __construct($image, $title, $description){
+    $this->image = $image;
+    $this->title = $title;
+    $this->description = $description;
+  }
+
+  public function getImage(){
+    return $this->image;
+}
+
+// Metodo per ottenere il titolo del film:
+
+public function getTitle(){
+    return $this->title;
+}
+
+// Metodo per ottenere la descrizione del film:
+
+public function getDescription(){
+    return $this->description;
+}
+}
